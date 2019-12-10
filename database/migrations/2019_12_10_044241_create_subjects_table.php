3a9951b7df28b1540e0e2d2ac51b3275d4f6584b
sebastian <sebastian.villadam@autonoma.edu.co>
2019-12-09 23:51:10 -0500

    db migrations created

---
+++ database/migrations/2019_12_10_044241_create_subjects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('name');
+            $table->Integer('responce_time');
+            $table->date('open_date');
+            $table->date('close_date');
+            $table->unsignedBigInteger('professor_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
---
+++ database/migrations/2019_12_10_044729_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('body');
+            $table->String('option_a')->nullable();
+            $table->String('option_b')->nullable();
+            $table->String('option_c')->nullable();
+            $table->String('option_d')->nullable();            
+            $table->String('correct');
+            $table->unsignedBigInteger('subject_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
