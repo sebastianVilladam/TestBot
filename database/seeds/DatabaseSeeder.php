ea21aba19471526287dd7b7330eb3812ce4f36bd
sebastian <sebastian.villadam@autonoma.edu.co>
2019-12-11 10:08:53 -0500

    seeder call fixe

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(QuestionsSeeder::class);
         $this->call(ProfessorsSeeder::class);
+        $this->call(AdministratorsSeeder::class);
     }
 }
