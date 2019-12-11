92cc89677b10d32833ab8a17a59bbccee1f0bfea
sebastian <sebastian.villadam@autonoma.edu.co>
2019-12-11 16:04:03 -0500

    Subjects controller created

---
+++ app/Http/Controllers/SubjectsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Subject;
+
+class SubjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subjects = Subject::all();
+        return view('privateViews.professors', ['list' => $subjects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('privateViews.professorRegister');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Subject::create($input);
+
+        $subjects = professor::all();
+        return view('privateViews.professors', ['list' => $subjects]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $subject = Subject::findOrFail($id);
+        return view('privateViews.professor', ['data' => $subject]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $subject = Subject::findOrFail($id);
+        return view('privateViews.professorSettings', ['data' => $subject]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $subject = Subject::findOrFail($id);
+        $input = $request->all();
+        $subject->fill($input)->save();
+        
+        return view('privateViews.professor', ['data' => $subject]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $subject = Subject::findOrFail($id);
+        $subject->delete();
+
+        $subjects = Subject::all();
+        return view('privateViews.professors', ['list' => $subjects]);
+    }
+}
