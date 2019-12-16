0395692d36c0c7d013e47df421e17e0a864075a4
sebastian <sebastian.villadam@autonoma.edu.co>
2019-12-16 10:03:04 -0500

    professors welcome page and layout

---
+++ resources/views/professorsViews/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.professorsLayout')
+@section('links')
+<link rel="stylesheet" href="css/welcome_style.css">
+@stop
+@section('content')
+<div id="titles">
+    <h1>Bienvenido!!</h1>
+    <h2>Docente 1</h2>
+</div>
+@stop
